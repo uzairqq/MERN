daf7440efea03a946c2300685854faa764f24292
uzairqq <uzair.qq@outlook.com>
2017-07-05 00:11:22 +0500

    separate jsx file to serve jsx & call from index.html

---
+++ Chapter2/public/App.jsx
@@ -0,0 +1,3 @@
+var contentNode=document.getElementById('contents');
+var component=<h1>Hello World Mern Uzair</h1>;
+ReactDOM.render(component,contentNode);
--- Chapter2/server.js
+++ Chapter2/server.js
@@ -1,9 +1,9 @@
 const  express=require('express');
 const app=express();
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 //app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use('/static',express.static('public'));
+//app.use('/static',express.static('public'));
 app.listen(3000, function () {
  console.log('App started on port 3000');
 });
