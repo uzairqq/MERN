bc357f2782cae4559d7f4e7b0b35e731833371d6
uzairqq <uzair.qq@outlook.com>
2017-07-05 17:34:08 +0500

    automatically convert jsx to javasript by doing compile in scripts in project.json by doing npm run compile and then npm start

--- Chapter2/static/jsx/App.js
+++ Chapter2/static/jsx/App.js
@@ -2,6 +2,6 @@ var contentNode = document.getElementById('contents');
 var component = React.createElement(
   'h1',
   null,
-  'Hello World Mern Uzair'
+  'Hello World Mern Uzair !! how are you .how do you do'
 );
 ReactDOM.render(component, contentNode);
