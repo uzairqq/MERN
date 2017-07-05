bc25bae7f4ea649ec5c37d744a9c362cf03f756b
uzairqq <uzair.qq@outlook.com>
2017-07-05 15:53:34 +0500

    changing jsx to javascript removing transform

--- Chapter2/server.js
+++ Chapter2/server.js
@@ -1,7 +1,7 @@
 const  express=require('express');
 const app=express();
 
-app.use(express.static('public'));
+app.use(express.static('static/jsx'));
 //app.use('/static', express.static(path.join(__dirname, 'public')))
 //app.use('/static',express.static('public'));
 app.listen(3000, function () {
---
+++ Chapter2/static/jsx/App.js
@@ -0,0 +1,7 @@
+var contentNode = document.getElementById('contents');
+var component = React.createElement(
+  'h1',
+  null,
+  'Hello World Mern Uzair'
+);
+ReactDOM.render(component, contentNode);
