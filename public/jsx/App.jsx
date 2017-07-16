131e07da6bff79d5b8a48c27a61042a783d4f27b
uzairqq <uzair.qq@outlook.com>
2017-07-17 00:48:56 +0500

    Dynamic composition With array of issue

--- public/jsx/App.jsx
+++ public/jsx/App.jsx
@@ -1,46 +1,86 @@
-const contentNode=document.getElementById('contents');
+const contentNode = document.getElementById('contents');
 
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the Issue Filter.</div>
+    )
+  }
+}
 
-
-
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+        <td>{issue.title}</td>
+      </tr>
+    )
+  }
+}
 
 class IssueTable extends React.Component {
- render() {
- const borderedStyle = {border: "1px solid silver", padding: 6};
- return (
- <table style={{borderCollapse: "collapse"}}>
- <thead>
- <tr>
- <th style={borderedStyle}>Id</th>
- <th style={borderedStyle}>Title</th>
- </tr>
- </thead>
- <tbody>
- <IssueRow issue_id={1}
- issue_name="Error in console when clicking Add" />
- <IssueRow issue_id={2}
- issue_name="Missing bottom border on panel" />
-  <IssueRow issue_id={3}
- issue_name="competitor services are not working " />
- </tbody>
- </table>
- )
- }
-}
-class IssueRow extends React.Component
-{
-    render()
-    {
-        const borderedStyle={border:"1px solid silver",padding:4};
-        return(
-            <tr>
-            <td style={borderedStyle}>{this.props.issue_id}</td>
-            <td style={borderedStyle}>{this.props.issue_name}</td>
-            </tr>
-            
-        )
-    }
+  render() {
+    const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
+    return (
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Completion Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    )
+  }
 }
 
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for an Issue Add entry form.</div>
+    )
+  }
+}
 
-ReactDOM.render(<IssueTable/>,contentNode);
+const issues = [
+  {
+    id: 1, status: 'Open', owner: 'Ravan',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+];
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);    // Render the component inside the content Node
