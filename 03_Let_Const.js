6d7d1eeca8bf4d593383009d47c4f31a6dff7f36
Uzair Iqbal <uzair.qq@outlook.com>
2025-10-02 18:03:13 +0500

    Step 2 Part B: let vs const example

---
+++ 03_Let_Const.js
@@ -0,0 +1,13 @@
+//Let
+let city = "karachi";
+console.log("City Before:", city);
+
+city = "Lahore";
+console.log("City After", city);
+
+//Const
+const country = "Pakistan";
+console.log("Before Country", country);
+
+country = "India";
+console.log("After Country", country);
