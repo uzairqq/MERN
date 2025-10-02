44a7ab9420386a5e129541ab169e8fc7806c6082
Uzair Iqbal <uzair.qq@outlook.com>
2025-10-02 18:24:32 +0500

    Step 2 Part C: null, undefined, typeof example

---
+++ 04_null_undefined_typeof.js
@@ -0,0 +1,15 @@
+let name;
+console.log("Name", name); //it will out the name undefined
+
+let age = null;
+console.log("Age", age);
+
+let country = "Pakistan";
+let founded = 1947;
+let isCountry = true;
+let city;
+
+console.log("Type Of", typeof country);
+console.log("Type Of", typeof founded);
+console.log("Type Of", typeof isCountry);
+console.log("Type Of", typeof city);
